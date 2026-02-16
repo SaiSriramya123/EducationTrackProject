cfc896369aecb2fa80e5ac6c4a31aefa184a6672
Sai Sriramya Setti <2471877@cognizant.com>
2026-02-16 20:13:25 +0530

    k

---
+++ source/repos/EducationTrackProject/EducationTrackProject/Models/Module.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationTrackProject.Models
+{
+	//[Table("Module")]
+	public class Module
+	{
+		[Key]
+		public string ModuleID { get; set; }
+
+		[Required]
+		[ForeignKey("Course")]
+		public string CourseID { get; set; }
+		//public Course Course { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public int SequenceOrder { get; set; }
+		public string LearningObjectives { get; set; }
+		//public ICollection<Content> Contents { get; set; }
+	}
+}
