a0d0353431d4d5e7f5914241da1ffb226f53eb8a
Jyothirmyee <2471876@cognizant.com>
2026-02-17 09:49:49 +0530

    get b

--- source/repos/EducationTrackProject/EducationTrackProject/obj/Debug/net10.0/EducationTrackProject.AssemblyInfo.cs
+++ source/repos/EducationTrackProject/EducationTrackProject/obj/Debug/net10.0/EducationTrackProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationTrackProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc896369aecb2fa80e5ac6c4a31aefa184a6672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006b3238a27071e13b999ff01cf7cc63d09e11bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationTrackProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationTrackProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
