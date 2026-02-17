bde50474a59275a809b3a9585dee565f91ec5ce5
AvulaLaxmiPrasanna <2471791@cognizant.com>
2026-02-17 13:45:34 +0530

    added classes

---
+++ Models/AcademicReport.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationTrackProject.Models
+{
+    public class AcademicReport
+    {
+        [Key]
+        public int ReportId { get; set; }
+        public string Scope { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal CompletionRate { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal AvgScore { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal DropOutRate { get; set; }
+
+        public DateTime GeneratedDate { get; set; }
+
+    }
+}
+
---
+++ Models/performance.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducationTrackProject.Models
+{
+    public class performance
+    {
+        
+            [Key]
+            public int ProgressID { get; set; }  // unique code for each course
+
+            public int EnrollmentId { get; set; } //id for student aftr enrooling, with this we can get details
+            [Column(TypeName = "decimal(5,2)")]
+            public decimal CompletionPercentage { get; set; }
+
+            public DateTime LastUpdated { get; set; }
+            [Column(TypeName = "decimal(5,2)")]
+            public decimal AvgScore { get; set; }
+
+        }
+    }
+
